46a00a93a173b348a02db4891dd67230bd381b3e
BrunoMagno <bmagnoserver@gmail.com>
2025-04-06 14:45:31 -0300

    Initial Structure

---
+++ src/main/java/com/archivumlibris/ArchivumLibrisApplication.java
@@ -0,0 +1,13 @@
+package com.archivumlibris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivumLibrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivumLibrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archivumlibris/ArchivumLibrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archivumlibris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivumLibrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
