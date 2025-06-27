c4637390a3a48acfb8b1a8bf1aa8a7232e59f714
Bruno Magno <bmagnoserver@gmail.com>
2025-06-27 16:24:23 -0300

    refactor: standardize JWT issuer and improve token generation logic in TokenService

--- src/main/java/com/archivumlibris/security/jwt/TokenService.java
+++ src/main/java/com/archivumlibris/security/jwt/TokenService.java
@@ -1,9 +1,7 @@
 package com.archivumlibris.security.jwt;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Date;
+import java.time.temporal.ChronoUnit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,6 +19,8 @@ public class TokenService {
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
+    private static final String ISSUER = "archivum-libris-api";
+
     public String createToken(String email, String password,
             AuthenticationManager authenticationManager) {
         try {
@@ -35,8 +35,8 @@ public class TokenService {
     private String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
-            return JWT.create().withIssuer("api-v1-auth").withClaim("id", user.getId())
-                    .withClaim("email", user.getEmail())
+            return JWT.create().withIssuer(ISSUER).withSubject(user.getEmail())
+                    .withClaim("id", user.getId()).withClaim("email", user.getEmail())
                     .withArrayClaim("roles", new String[] {user.getRole().name()})
                     .withIssuedAt(genIssuedAtDate()).withExpiresAt(genExpirationDate())
                     .sign(algorithm);
@@ -46,23 +46,18 @@ public class TokenService {
     }
 
     private Instant genIssuedAtDate() {
-        return LocalDateTime.now().toInstant(ZoneOffset.of("-03:00"));
+        return Instant.now().minusSeconds(5);
     }
 
     private Instant genExpirationDate() {
-        return LocalDateTime.now().plusDays(7).toInstant(ZoneOffset.of("-03:00"));
+        return Instant.now().plus(2, ChronoUnit.HOURS);
     }
 
     public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
 
-            DecodedJWT decodedJWT =
-                    JWT.require(algorithm).withIssuer("api-v1-auth").build().verify(token);
-
-            if (decodedJWT.getExpiresAt().before(new Date())) {
-                throw new RuntimeException("Token expired");
-            }
+            DecodedJWT decodedJWT = JWT.require(algorithm).withIssuer(ISSUER).build().verify(token);
 
             return decodedJWT.getClaim("email").asString();
 
