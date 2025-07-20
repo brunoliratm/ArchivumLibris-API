e95ae34ea18f18fb0e505ee236260e780384ac47
Bruno Magno <bmagnoserver@gmail.com>
2025-07-19 23:26:02 -0300

    feat: enforce NOT NULL constraints and add CHECK constraints for genre and payment methods in database schema

--- src/main/resources/db/migration/V3__create_purchase_table.sql
+++ src/main/resources/db/migration/V3__create_purchase_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE purchases (
     id BIGSERIAL PRIMARY KEY,
-    purchase_date DATE,
-    pay_method VARCHAR(255),
-    price NUMERIC(19, 2),
-    book_id BIGINT,
-    user_id BIGINT,
+    purchase_date DATE NOT NULL,
+    pay_method VARCHAR(20) NOT NULL CHECK (pay_method IN ('CREDIT_CARD', 'DEBIT_CARD', 'PIX', 'PAYPAL', 'BOLETO', 'OTHER')),
+    price NUMERIC(19, 2) NOT NULL,
+    book_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     FOREIGN KEY (book_id) REFERENCES books(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
